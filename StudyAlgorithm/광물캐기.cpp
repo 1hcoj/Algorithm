ff20914c3a4cfe19e606fc4f295cfb36adbeb65f
chihyeon <chi2941@naver.com>
2023-07-31 14:00:27 +0900

    0731 commit

--- StudyAlgorithm/광물캐기.cpp
+++ StudyAlgorithm/광물캐기.cpp
@@ -1,52 +1,52 @@
-//#include <string>
-//#include <vector>
-//#include <queue>
-//
-//using namespace std;
-//
-//int solution(vector<int> picks, vector<string> minerals) {
-//    int answer = 0;
-//    int pick_num = 0;
-//    priority_queue<pair<int,int>> scores;
-//    for (int pick : picks) {
-//        pick_num += pick;
-//    }
-//    for (int i = 0; i < pick_num * 5 && i < minerals.size(); i += 5) {
-//        int sum = 0;
-//        for (int j = 0; j < 5; j++) {
-//            if (i + j < minerals.size()) {
-//                if (minerals[i + j] == "diamond") sum += 1;
-//                else if (minerals[i + j] == "stone") sum -= 1;
-//            }
-//            else {
-//                break;
-//            }
-//        }
-//        scores.push({ sum,i });
-//    }
-//    while (!scores.empty()) {
-//        pair<int, int> score = scores.top(); scores.pop();
-//        int index = score.second;
-//        int pick = -1;
-//        for (int k = 0; k < 3; k++) {
-//            if (picks[k]) {
-//                pick = k;
-//                picks[k]--;
-//                break;
-//            }
-//        }
-//        for (int i = index; i < minerals.size() && i < index + 5; i++) {
-//            if (minerals[i] == "diamond") {
-//                answer += pow(5, pick);
-//            }
-//            else if (minerals[i] == "iron") {
-//                pick = (pick == 0) ? pick : pick - 1;
-//                answer += pow(5, pick);
-//            }
-//            else {
-//                answer += 1;
-//            }
-//        }
-//    }
-//    return answer;
-//}
+#include <string>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+int ±¤¹°Ä³±â(vector<int> picks, vector<string> minerals) {
+    int answer = 0;
+    int pick_num = 0;
+    priority_queue<pair<int,int>> scores;
+    for (int pick : picks) {
+        pick_num += pick;
+    }
+    for (int i = 0; i < pick_num * 5 && i < minerals.size(); i += 5) {
+        int sum = 0;
+        for (int j = 0; j < 5; j++) {
+            if (i + j < minerals.size()) {
+                if (minerals[i + j] == "diamond") sum += 1;
+                else if (minerals[i + j] == "stone") sum -= 1;
+            }
+            else {
+                break;
+            }
+        }
+        scores.push({ sum,i });
+    }
+    while (!scores.empty()) {
+        pair<int, int> score = scores.top(); scores.pop();
+        int index = score.second;
+        int pick = -1;
+        for (int k = 0; k < 3; k++) {
+            if (picks[k]) {
+                pick = k;
+                picks[k]--;
+                break;
+            }
+        }
+        for (int i = index; i < minerals.size() && i < index + 5; i++) {
+            if (minerals[i] == "diamond") {
+                answer += pow(5, pick);
+            }
+            else if (minerals[i] == "iron") {
+                pick = (pick == 0) ? pick : pick - 1;
+                answer += pow(5, pick);
+            }
+            else {
+                answer += 1;
+            }
+        }
+    }
+    return answer;
+}
--- StudyAlgorithm/요격시스템.cpp
+++ StudyAlgorithm/요격시스템.cpp
@@ -1,39 +1,39 @@
-//#include <string>
-//#include <vector>
-//
-//using namespace std;
-//
-//int solution(vector<vector<int>> targets) {
-//    int answer = 0;
-//    vector<vector<int>> sections;
-//    for (vector<int> target : targets) {
-//        if (sections.empty()) sections.push_back(target);
-//        else {
-//            bool toggle = true;
-//            for (vector<int>& section : sections) {
-//                if (target[0] <= section[0] && target[1] >= section[1]) {
-//                    toggle = false;
-//                    break;
-//                }
-//                else if (target[0] >= section[0] && target[1] <= section[1]) {
-//                    section = target; 
-//                    toggle = false;
-//                    break;
-//                }
-//                else if (target[0] >= section[0]) {
-//                    section = { target[0],section[1] };
-//                    toggle = false;
-//                    break;
-//                }
-//                else if (target[1] <= section[1]) {
-//                    section = { section[0],target[1] };
-//                    toggle = false;
-//                    break;
-//                }
-//            }
-//            if (toggle) sections.push_back(target);
-//        }
-//    }
-//    answer = sections.size();
-//    return answer;
-//}
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int 요격시스템(vector<vector<int>> targets) {
+    int answer = 0;
+    vector<vector<int>> sections;
+    for (vector<int> target : targets) {
+        if (sections.empty()) sections.push_back(target);
+        else {
+            bool toggle = true;
+            for (vector<int>& section : sections) {
+                if (target[0] <= section[0] && target[1] >= section[1]) {
+                    toggle = false;
+                    break;
+                }
+                else if (target[0] >= section[0] && target[1] <= section[1]) {
+                    section = target; 
+                    toggle = false;
+                    break;
+                }
+                else if (target[0] >= section[0]) {
+                    section = { target[0],section[1] };
+                    toggle = false;
+                    break;
+                }
+                else if (target[1] <= section[1]) {
+                    section = { section[0],target[1] };
+                    toggle = false;
+                    break;
+                }
+            }
+            if (toggle) sections.push_back(target);
+        }
+    }
+    answer = sections.size();
+    return answer;
+}
